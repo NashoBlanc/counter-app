7e12f45a5f5a7b01c57b6101540eafccdd47c18d
ignacioblanc <iiblanc@falabella.cl>
2021-05-12 18:11:38 -0400

    first hi world on React!

---
+++ src/FirstApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FirstApp = ()  => {
+
+    return  (
+    <>
+        <h1>Hi world</h1>
+        <p>My first app</p>
+    </>
+    );
+
+
+}
+
+
+
+export default FirstApp;
