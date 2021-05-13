4b56370947edab6262fb7ad3d05ab10d4a133226
ignacioblanc <iiblanc@falabella.cl>
2021-05-13 11:57:11 -0400

    added css and example with onclick event from button

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import FirstApp from './FirstApp';
 import CounterApp from './CounterApp';
+import './index.css';
+
 
 const divRoot = document.querySelector('#root');
 
