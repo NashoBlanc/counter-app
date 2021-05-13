c423a1c5b337beea4727b3727be02e3e85b20044
ignacioblanc <iiblanc@falabella.cl>
2021-05-13 12:13:46 -0400

    fuctions example with counter value

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value })  => {
 
+    const [counter, setcounter] = useState(0);
+
     const handleAdd = (e) => { 
-        console.log(e);
+        setcounter((c) => c + 1);
+    }
+
+    const handleSubstract = () => setcounter((c) => c - 1);
+    
+    const handleReset = () => { 
+        setcounter((c) => c = 0);
     }
 
     return  (
     <>
         <h1>CounterApp</h1>
-        <h2>{ value } </h2>
+        <h2>{ counter } </h2>
         <button onClick={ handleAdd }>+1</button>
+        <button onClick={ handleSubstract }>-1</button>
+        <button onClick={ handleReset }>Reset</button>
     </>
     );
 }
